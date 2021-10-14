fb609bedc4ba7939d74ff1f3ff79fff886894f62
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-10-14 13:07:40 -0600

    Decreasing Multiples of 3

--- Loop_Challenges.js
+++ Loop_Challenges.js
@@ -1,7 +1,15 @@
 
+/*
 //Print odd 1-20
 for( var i = 1; i <21; i++){
     if( i % 2 != 0){
         console.log(i);
+    }
+}
+*/
+//Decreasing Multiples of 3
+for( i = 100; i >= 0; i--){
+    if( i % 3 == 0){
+        console.log
     }
 }
