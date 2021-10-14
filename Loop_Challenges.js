8cf8371af900b21800616e47f4e7408f7537d769
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-10-14 13:12:06 -0600

    Print the Sequence

--- Loop_Challenges.js
+++ Loop_Challenges.js
@@ -6,10 +6,17 @@ for( var i = 1; i <21; i++){
         console.log(i);
     }
 }
-*/
+
 //Decreasing Multiples of 3
 for( i = 100; i >= 0; i--){
     if( i % 3 == 0){
         console.log
     }
+}
+*/
+//Print the Sequence
+var arr = [4,2.5,1,-0.5,-2,-3.5];
+
+for( i = 0; i < arr.length; i++){
+    console.log([i]);
 }
